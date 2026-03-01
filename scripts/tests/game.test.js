e681de76b21cffa764dc6525a213915d7300c6ca
CtrlAltKismet <Brookesdet@gmail.com>
2026-03-01 18:18:57 +0000

    Created new game function.

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 
-const { game } = require("../game");
+const { game, newGame } = require("../game");
 
 beforeAll(() => {
     let fs = require('fs');
@@ -27,3 +27,14 @@ describe("game object contains correct keys", () => {
         expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
     });
 });
+
+describe("newGame works correctly", () => {
+    beforeAll(() => {
+        game.score = 42;
+        newGame();
+    });
+    test("should set game score to 0", () => {
+        expect(game.score).toEqual(0);
+    });
+
+    });
